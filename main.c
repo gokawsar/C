8052d5b26077a4a17f14ddc54bd8c8906447755b
Md. Kawsar Ahmed <115777747+gokawsar@users.noreply.github.com>
2022-12-09 12:17:05 +0600

    Update main.c

--- main.c
+++ main.c
@@ -4,20 +4,21 @@ int ht, run, score;
 
 int toss(){
 
-printf("You want to Bat(1) or Ball(2) \n-------[Enter 1 or 2]-------\n");
+printf("\nYou want to Bat(1) or Ball(2) \n-------[Enter 1 or 2]-------\n");
 scanf("%d", &ht);
+                if (ht==1)
+                    printf("\nYou Chose Batting\n");
+                else if (ht==2)
+                    printf("\nYou Chose Bowling\n");
+                else{
+                    toss();
+                    }
 return 0;
 
 }
 
 int main(){
             toss();
-                    if (ht==1)
-                        printf("\nYou Chose Batting\n");
-                    else if (ht==2)
-                        printf("\nYou Chose Bowling\n");
-                    else
-                        printf("\nSelect Batting(1) or Bowling(2)\n");
 
 
 int hit = rand() % 7;
